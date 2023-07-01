88ae989fb1334d73d9e5c387cffb362cd1e580e1
silviafds <silviafernandes974@gmail.com>
2023-07-01 17:39:47 -0300

    API REST

---
+++ previsao_do_tempo/src/main/java/com/previsao_do_tempo/previsao_do_tempo/PrevisaoDoTempoApplication.java
@@ -0,0 +1,13 @@
+package com.previsao_do_tempo.previsao_do_tempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevisaoDoTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevisaoDoTempoApplication.class, args);
+	}
+
+}
---
+++ previsao_do_tempo/src/test/java/com/previsao_do_tempo/previsao_do_tempo/PrevisaoDoTempoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.previsao_do_tempo.previsao_do_tempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevisaoDoTempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
