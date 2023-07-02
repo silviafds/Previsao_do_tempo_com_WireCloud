64f3803d2475cfaccd100232233a022bfbe92b8b
silviafds <silviafernandes974@gmail.com>
2023-07-01 22:10:21 -0300

    feat: api retorna os dados

--- previsao_do_tempo/src/main/java/com/previsao_do_tempo/previsao_do_tempo/service/previsaoService.java
+++ previsao_do_tempo/src/main/java/com/previsao_do_tempo/previsao_do_tempo/service/previsaoService.java
@@ -3,6 +3,15 @@ package com.previsao_do_tempo.previsao_do_tempo.service;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,21 +24,56 @@ public class previsaoService {
     }
 
     public String dadosCidade(String city) throws JSONException {
-        String cidade=null;
-        if (city!=null) { //convete dado para String
-            JSONObject jsonObject = new JSONObject(city);
-            cidade = jsonObject.getString("cidade");
-        }
 
-        //chama API
-        String apiURL = "https://api.openweathermap.org/data/3.0/onecall?q="+cidade+"&units=metric&appid="+apiKey+"&lang=pt_br";
+        String lugar = converteString(city);
 
-        if(apiURL==null) {
-            return "é null";
-        }
-        System.out.println(apiURL);
-        return apiURL;
+        String x = acessaAPI(lugar);
+
+        return x;
     }
 
+    /*
+    *  converte JSON para String
+    */
+    public String converteString(String city) throws JSONException {
+        String cidade;
+        if (city!=null) {
+            JSONObject jsonObject = new JSONObject(city);
+            cidade = jsonObject.getString("cidade");
+            return cidade;
+        }
+        return "nulo";
+    }
+
+    /*
+     *  converte JSON para String
+     */
+    private String acessaAPI(String lugar) {
+        List<String> listaDeDadosTempo = new ArrayList<String>();
+        String linha;
+
+        try {
+            // Criar uma URL
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q="+lugar+"&units=metric&appid="+apiKey+"&lang=pt_br");
+
+            // Abrir conexão
+            URLConnection connection = url.openConnection();
+
+            // Criar um BufferedReader para ler a resposta
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            // Ler e exibir o conteúdo da URL linha por linha
+            while ((linha = reader.readLine()) != null) {
+                System.out.println(linha);
+            }
+
+            reader.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lugar;
+    }
 
 }
