9e4fc919a2d23d7a6c94c524f9804aef767e3773
silviafds <silviafernandes974@gmail.com>
2023-07-02 13:57:41 -0300

    feat: função que arrendonda e converte numero para inteiro adicionada

--- previsao_do_tempo/src/main/java/com/previsao_do_tempo/previsao_do_tempo/controller/previsaoController.java
+++ previsao_do_tempo/src/main/java/com/previsao_do_tempo/previsao_do_tempo/controller/previsaoController.java
@@ -3,7 +3,6 @@ package com.previsao_do_tempo.previsao_do_tempo.controller;
 import com.previsao_do_tempo.previsao_do_tempo.service.previsaoService;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,12 +13,6 @@ public class previsaoController {
 
     @Autowired
     private previsaoService prev;
-
-    @GetMapping
-    public String eita() {
-        String palavra = prev.teste();
-        return palavra;
-    }
 
     @RequestMapping(value = "/cidade", method =  RequestMethod.POST)
     public List<Object> inserirCidade(@RequestBody String cidade) throws JSONException {
--- previsao_do_tempo/src/main/java/com/previsao_do_tempo/previsao_do_tempo/service/previsaoService.java
+++ previsao_do_tempo/src/main/java/com/previsao_do_tempo/previsao_do_tempo/service/previsaoService.java
@@ -21,16 +21,10 @@ public class previsaoService {
 
     public String apiKey = "80e0070b689cbee079cb225a96f68b9a";//adicinar chave aqui
 
-    public String teste() {
-        return "testando app service";
-    }
-
     public List<Object> dadosCidade(String city) throws JSONException {
         List<Object> dadosLugares;
         String lugar = converteString(city);
-
         dadosLugares = acessaAPI(lugar);
-
         return dadosLugares;
     }
 
@@ -48,7 +42,7 @@ public class previsaoService {
     }
 
     /*
-     *  converte JSON para String
+     *  Acessa API e retorna list com os dados necessários de previsão do tempo
      */
     private List<Object> acessaAPI(String lugar) {
         String linha;
@@ -101,14 +95,14 @@ public class previsaoService {
             valores = new ArrayList<>();
             valores.add(lugar); //lugar
             valores.add(country); //país
-            valores.add(temp); //temperatura atual
+            valores.add(arredondarEConverterParaInteiro(temp)); //temperatura atual
             valores.add(main); // status atual: chuva, sol, nuvens
-            valores.add(feelsLike); // sensação termica
-            valores.add(tempMin); //temperatura minima
-            valores.add(tempMax); //temperatura maxima
+            valores.add(arredondarEConverterParaInteiro(feelsLike)); // sensação termica
+            valores.add(arredondarEConverterParaInteiro(tempMin)); //temperatura minima
+            valores.add(arredondarEConverterParaInteiro(tempMax)); //temperatura maxima
             valores.add(description); //description
-            valores.add(humidity); //umidade
-            valores.add(speed); //velociade vento
+            valores.add(arredondarEConverterParaInteiro(humidity)); //umidade
+            valores.add(arredondarEConverterParaInteiro(speed)); //velociade vento
             valores.add(pressure); // pressao
             valores.add(lon); //longitude
             valores.add(lat); //latitude
@@ -123,6 +117,17 @@ public class previsaoService {
         }
 
         return valores;
+    }
+
+
+    /*
+     *  Função que arredonda e converte para inteiro
+     */
+    public static int arredondarEConverterParaInteiro(double numero) {
+        long numeroArredondado = Math.round(numero);
+        int numeroInteiro = Math.toIntExact(numeroArredondado);
+
+        return numeroInteiro;
     }
 
 
